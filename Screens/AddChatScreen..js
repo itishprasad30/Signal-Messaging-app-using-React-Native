acf242d3b573f49619602d13fc9ed127136b9ad6
itishprasad30 <itishprasad20@gmail.com>
2021-09-15 20:46:37 +0530

    ⭐All Screen componets are added

---
+++ Screens/AddChatScreen..js
@@ -0,0 +1,45 @@
+import React, { useLayoutEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input } from "react-native-elements/";
+import { Icon } from "react-native-elements/";
+import { db } from "../firebase";
+
+const AddChatScreen = ({ navigation }) => {
+  const [input, setInput] = useState("");
+
+  const createChat = async () => {
+    await db
+      .collection("chats")
+      .add({ chatName: input })
+      .then(() => {
+        navigation.goBack();
+      })
+      .catch((error) => alert(error));
+  };
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a new Chat",
+      headerBackTitle: "chats",
+    });
+  }, [navigation]);
+  return (
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a chat name"
+        value={input}
+        onSubmitEditing={createChat}
+        onChangeText={(text) => setInput(text)}
+        leftIcon={
+          <Icon name="wechat" type="antdesign" size={24} color="black" />
+        }
+      />
+      <Button disabled={!input} onPress={createChat} title="Create a chat" />
+    </View>
+  );
+};
+
+export default AddChatScreen;
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: "white", padding: 30, height: "100%" },
+});
---
+++ Screens/HomeScreen.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { SafeAreaView } from "react-native";
+import { TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { Avatar } from "react-native-elements/dist/avatar/Avatar";
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
+import CustomListItem from "../Components/CustomListItem";
+import { auth, db } from "../firebase";
+
+const HomeScreen = ({ navigation }) => {
+  const [chats, setChats] = useState([]);
+
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      navigation.replace("Login");
+    });
+  };
+  useEffect(() => {
+    const unsubscribe = db.collection("chats").onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+    return unsubscribe;
+  }, []);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Signal",
+      headerStyle: { backgroundColor: "#2C6EBD" },
+      headerTitleStyle: { color: "white" },
+      headerTintColor: "white",
+      headerLeft: () => (
+        <View style={{ marginLeft: 20 }}>
+          <TouchableOpacity onPress={signOutUser} activeOpacity={0.5}>
+            <Avatar rounded source={{ uri: auth?.currentUser?.photoUrl }} />
+          </TouchableOpacity>
+        </View>
+      ),
+
+      headerRight: () => (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 80,
+            marginRight: 20,
+          }}
+        >
+          <TouchableOpacity activeOpacity={0.5}>
+            <AntDesign name="camerao" size={24} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.5}
+            onPress={() => navigation.navigate("AddChat")}
+          >
+            <SimpleLineIcons name="pencil" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, [navigation]);
+
+  const enterChat = (id, chatName) => {
+    navigation.navigate("Chat", {
+      id,
+      chatName,
+    });
+  };
+  return (
+    <SafeAreaView>
+      <ScrollView style={styles.container}>
+        {chats.map(({ id, data: { chatName } }) => (
+          <CustomListItem
+            key={id}
+            id={id}
+            chatName={chatName}
+            enterChat={enterChat}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+});
